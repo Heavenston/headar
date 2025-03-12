73153f7f5e93b08cc30ee2336f9e4191c7c6a2de
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2025-03-12 16:49:50 +0100

    feat: Interface a peu pret fairet

--- header-front/src/App.tsx
+++ header-front/src/App.tsx
@@ -1,13 +1,43 @@
-import { For, type Component } from 'solid-js';
-import { eachDayOfInterval, eachWeekOfInterval, endOfMonth, endOfWeek, isWithinInterval, setMonth, startOfMonth } from "date-fns";
+import { createSignal, For, onCleanup, type Component } from 'solid-js';
+import { eachDayOfInterval, eachWeekOfInterval, endOfMonth, endOfWeek, Interval, isSameDay, isWithinInterval, setMonth, startOfMonth } from "date-fns";
 
 const App: Component = () => {
+  const controller = new AbortController();
+  onCleanup(() => {
+    controller.abort();
+  });
+
   const base_date = new Date(2025, 1, 1, 1);
   const months = [5, 6, 7, 8];
   const intl = Intl.DateTimeFormat("fr-FR", {
     timeZone: "Europe/Paris",
     month: "long",
   });
+
+  const [currentSelection, setCurrentSelection] = createSignal<Interval | null>(null);
+  const [isSelecting, setIsSelecting] = createSignal(false);
+
+  function commitSelection() {
+    setIsSelecting(false);
+  }
+
+  function forgetSelection() {
+    setIsSelecting(false);
+    setCurrentSelection(null);
+  }
+
+  window.addEventListener("keydown", (ev: KeyboardEvent) => {
+    if (ev.code === "Escape") {
+      forgetSelection();
+      ev.preventDefault();
+    }
+  }, { signal: controller.signal });
+  window.addEventListener("mousedown", (me: MouseEvent) => {
+    if (isSelecting()) {
+      forgetSelection();
+      me.preventDefault();
+    }
+  }, { signal: controller.signal });
 
   return (
     <div>
@@ -24,16 +54,57 @@ const App: Component = () => {
                 children={week => (<div class={`gap-3 flex flex-row`}>
                   <For
                     each={eachDayOfInterval({ start: week, end: endOfWeek(week, { weekStartsOn: 1 }) })}
-                    children={day => <span class={`
-                      inline-block w-25 h-25 p-2 rounded
-                      bg-gray-200
-                      select-none
-                      ${isWithinInterval(day, monthInterval) ? "opacity-100" : "opacity-25"}
-                    `}>
-                      <span>
-                        {day.getDate()}
-                      </span>
-                    </span>}
+                    children={day => {
+                      const is_selected = () => currentSelection() && isWithinInterval(day, currentSelection()!);
+
+                      return <span
+                        class={`
+                          inline-block w-25 h-25 p-2 rounded
+                          bg-gray-200
+                          select-none
+                          ${is_selected() ? `bg-yellow-500` : ``}
+                          ${isWithinInterval(day, monthInterval) ? "opacity-100" : "opacity-25"}
+                        `}
+                        onMouseDown={e => {
+                          e.stopPropagation();
+                          if (isSelecting()) {
+                            setCurrentSelection({
+                              start: currentSelection()?.start ?? day,
+                              end: day,
+                            });
+                            setIsSelecting(false);
+                            commitSelection();
+                          }
+                          else {
+                            setCurrentSelection({
+                              start: day,
+                              end: day,
+                            });
+                            setIsSelecting(true);
+                          }
+                        }}
+                        onMouseEnter={() => {
+                          if (!isSelecting())
+                            return;
+                          setCurrentSelection({
+                            start: currentSelection()?.start ?? day,
+                            end: day,
+                          });
+                        }}
+                        onMouseUp={() => {
+                          if (!isSelecting())
+                            return;
+                          if (!currentSelection() || !isSameDay(day, currentSelection()?.start ?? day)) {
+                            setIsSelecting(false);
+                            commitSelection();
+                          }
+                        }}
+                      >
+                        <span>
+                          {day.getDate()}
+                        </span>
+                      </span>;
+                    }}
                   />
                 </div>)}
               />
