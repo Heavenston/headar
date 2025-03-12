0dfcf6ca46810f1f4d37df1b10d670a5e2a87bf1
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2025-03-12 22:30:15 +0100

    feat: Show best levels

--- header-front/src/Calendar.tsx
+++ header-front/src/Calendar.tsx
@@ -1,6 +1,7 @@
-import { createComputed, createMemo, createSignal, For, onCleanup, useContext, type Component } from 'solid-js';
+import { createComputed, createMemo, createSignal, For, onCleanup, Show, useContext, type Component } from 'solid-js';
 import { eachDayOfInterval, eachWeekOfInterval, endOfDay, endOfMonth, endOfWeek, Interval, isSameDay, isWithinInterval, setMonth, startOfDay, startOfMonth } from "date-fns";
 import { GlobalStore } from './App';
+import { RangeAvailability } from './spacetime_bindings';
 
 const Calendar: Component = () => {
   const store = useContext(GlobalStore);
@@ -22,6 +23,7 @@ const Calendar: Component = () => {
   const [currentSelection, setCurrentSelection] = createSignal<Interval | null>(null);
   const [isSelecting, setIsSelecting] = createSignal(false);
   const [availabilityLevel, setAvailabilityLevel] = createSignal(0);
+  const [tab, setTab] = createSignal<"global" | "personal">("personal");
 
   const commitSelection = () => {
     setIsSelecting(false);
@@ -38,10 +40,6 @@ const Calendar: Component = () => {
 
     store.connection.reducers.createAvailabilityRange(start.toISOString(), end.toISOString(), availabilityLevel());
     setCurrentSelection(null);
-
-    setTimeout(() => {
-      console.log([...store.connection.db.rangeAvailability.iter()]);
-    }, 250);
   };
 
   const forgetSelection = () => {
@@ -63,8 +61,8 @@ const Calendar: Component = () => {
   }, { signal: controller.signal });
 
   return (
-    <div class={`h-screen gap-10 flex flex-row justify-between`}>
-      <div class={`flex-grow p-10`}>
+    <div class={`h-screen gap-5 flex flex-row justify-between`}>
+      <div class={`flex-grow p-5 flex flex-col gap-5`}>
         <div>
           <span class="text-gray-600">Connected as </span>{store.users[store.user_id ?? 0]?.username}
           {" "}<button
@@ -86,31 +84,58 @@ const Calendar: Component = () => {
           </button>
         </div>
         
-        <div class="mt-10">
-          <h3 class="font-bold mb-3">Niveau de disponibilité</h3>
-          <div class="flex flex-col gap-3">
-            <div 
-              class={`p-3 rounded cursor-pointer ${availabilityLevel() === 0 ? 'ring-2 ring-blue-500' : ''}`}
-              style={{ background: '#f0a5a5' }}
-              onClick={() => setAvailabilityLevel(0)}
+        <div class={`flex flex-col gap-3`}>
+          <div class="flex w-full">
+            <button
+              class={`
+                flex-grow px-4 py-2 font-medium
+                ${tab() === "personal" ? "border-b-2 border-blue-500" : "border-b-2 border-gray-300"}
+                cursor-pointer
+              `}
+              onClick={() => setTab("personal")}
             >
-              PAS disponible
-            </div>
-            <div 
-              class={`p-3 rounded cursor-pointer ${availabilityLevel() === 1 ? 'ring-2 ring-blue-500' : ''}`}
-              style={{ background: '#f0d6a5' }}
-              onClick={() => setAvailabilityLevel(1)}
+              Personal Calendar
+            </button>
+            <button
+              class={`
+                flex-grow px-4 py-2 font-medium
+                ${tab() === "global" ? "border-b-2 border-blue-500" : "border-b-2 border-gray-300"}
+                cursor-pointer
+              `}
+              onClick={() => setTab("global")}
             >
-              Arrangeable
-            </div>
-            <div 
-              class={`p-3 rounded cursor-pointer ${availabilityLevel() === 2 ? 'ring-2 ring-blue-500' : ''}`}
-              style={{ background: '#a5f0aa' }}
-              onClick={() => setAvailabilityLevel(2)}
-            >
-              Devrait être disponible
-            </div>
+              Aggregate Calendar
+            </button>
           </div>
+
+          <Show when={tab() === "personal"}>
+            <div class="">
+              <h3 class="font-bold mb-3">Pinceau de niveau de disponibilité</h3>
+              <div class="flex flex-col gap-3">
+                <div 
+                  class={`p-3 rounded cursor-pointer ${availabilityLevel() === 0 ? 'ring-2 ring-blue-500' : ''}`}
+                  style={{ background: '#f0a5a5' }}
+                  onClick={() => setAvailabilityLevel(0)}
+                >
+                  PAS disponible
+                </div>
+                <div 
+                  class={`p-3 rounded cursor-pointer ${availabilityLevel() === 1 ? 'ring-2 ring-blue-500' : ''}`}
+                  style={{ background: '#f0d6a5' }}
+                  onClick={() => setAvailabilityLevel(1)}
+                >
+                  Arrangeable
+                </div>
+                <div 
+                  class={`p-3 rounded cursor-pointer ${availabilityLevel() === 2 ? 'ring-2 ring-blue-500' : ''}`}
+                  style={{ background: '#a5f0aa' }}
+                  onClick={() => setAvailabilityLevel(2)}
+                >
+                  Devrait être disponible
+                </div>
+              </div>
+            </div>
+          </Show>
         </div>
       </div>
       <div class={`min-h-screen overflow-auto gap-10 flex flex-col p-10`}>
@@ -128,20 +153,26 @@ const Calendar: Component = () => {
                     each={eachDayOfInterval({ start: week, end: endOfWeek(week, { weekStartsOn: 1 }) })}
                     children={day => {
                       const is_selected = () => currentSelection() && isWithinInterval(day, currentSelection()!);
-                      const level = createMemo<number>(() => {
-                        for (const p of Object.values(store.range_availability)) {
-                          if (p == null)
-                            continue;
-                          if (p.creatorUserId !== store.user_id)
-                            continue;
-                          if (isWithinInterval(day, { start: p.rangeStart, end: p.rangeEnd }))
-                            return p.availabilityLevel;
-                        }
-                        return 0;
-                      });
+                      const myRanges = createMemo<RangeAvailability[]>(() => (
+                        Object.values(store.range_availability)
+                          .filter(p => p != null)
+                          .filter(p => isWithinInterval(day, { start: p.rangeStart, end: p.rangeEnd }))
+                      ));
+                      const packs = createMemo<[number, number, number]>(() => (
+                        myRanges().reduce((curr, ava) => {
+                          curr[ava.availabilityLevel]++;
+                          return curr;
+                        }, [0, 0, 0] as [number, number, number])
+                      ));
+                      const bestLevel = () => packs().reduce((best, _current, current, arr) => arr[current] > arr[best] ? current : best);
+                      const personalLevel = createMemo<number>(() => (
+                        myRanges().find(p => p.creatorUserId === store.user_id)?.availabilityLevel ?? 0
+                      ));
+
+                      const renderTargetLevel = () => tab() === "personal" ? personalLevel() : bestLevel();
 
                       return <span
-                        data-level={level()}
+                        data-level={personalLevel()}
                         class={`
                           inline-block w-25 h-25 p-2 rounded
                           bg-gray-200
@@ -150,8 +181,8 @@ const Calendar: Component = () => {
                           ${isWithinInterval(day, monthInterval) ? "opacity-100" : "opacity-25"}
                         `}
                         style={{
-                          background: level() === 0 ? '#f0a5a5' : 
-                                      level() === 1 ? '#f0d6a5' : 
+                          background: renderTargetLevel() === 0 ? '#f0a5a5' : 
+                                      renderTargetLevel() === 1 ? '#f0d6a5' : 
                                                      '#a5f0aa'
                         }}
                         oncontextmenu={e => {
